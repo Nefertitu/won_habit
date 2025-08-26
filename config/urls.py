160e77714660b2a365d65aaa05870fee14fb334f
Olga <nefertituolga@gmail.com>
2025-08-26 18:41:58 +0300

    add fixes config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -21,8 +21,8 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("users", include("users.urls")),
-    path("habits", include("habits.urls")),
+    path("users/", include("users.urls")),
+    path("habits/", include("habits.urls")),
     path("swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"),
     path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
